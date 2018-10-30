971c2a3058d2d62552eef110af608a7442a7f9d3
zitt <NKtargeting@yandex.ru>
2018-10-30 23:45:25 +0500

    Implement list instance

--- src/main/scala/fintech/homework06/Eq.scala
+++ src/main/scala/fintech/homework06/Eq.scala
@@ -16,6 +16,16 @@ object EqInstances {
   implicit val intInstance: Eq[Int] = atomInstance[Int]
   implicit val stringInstance: Eq[String] = atomInstance[String]
   implicit val noneInstance: Eq[None.type] = atomInstance[None.type]
+
+  implicit def listInstance[T: Eq]: Eq[Seq[T]] = new Eq[Seq[T]] {
+    def equiv(lft: Seq[T], rgt: Any): Boolean = rgt match {
+      case x: Seq[T] =>
+        val eq = implicitly[Eq[T]]
+        lft.size == x.size && lft.zip(x).forall(pair => eq.equiv(pair._1, pair._2))
+      case _ =>
+        false
+    }
+  }
 }
 object EqSyntax {
   implicit class EqOps[T: Eq](self: T) {
