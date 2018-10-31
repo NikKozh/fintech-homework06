089b5f052f83840b0cb1ceb8103ac97c36e5e86e
zitt <NKtargeting@yandex.ru>
2018-10-31 22:37:32 +0500

    Clean up code and add comments

--- src/main/scala/fintech/homework06/Eq.scala
+++ src/main/scala/fintech/homework06/Eq.scala
@@ -6,20 +6,26 @@ package fintech.homework06
 */
 
 trait Eq[-T] {
+  /*
+  * Мне кажется, что тип правого операнда должен быть Any, т.к. в этом случае мы можем
+  * сравнить конкретный тип с чем угодно без ошибок компиляции (как это сделано для методов
+  * == и isEqual в стандартной библиотеке), при этом pattern-matching всё ещё позволит нам
+  * проводить корректное сравнение, если правый операнд такого же типа.
+  * */
   def equiv(lft: T, rgt: Any): Boolean
 }
 
-object EqInstances {
+trait EqInstances {
   def atomInstance[T]: Eq[T] = new Eq[T] {
     def equiv(lft: T, rgt: Any): Boolean = lft == rgt
   }
-  implicit val intInstance: Eq[Int] = atomInstance[Int]
-  implicit val stringInstance: Eq[String] = atomInstance[String]
-  implicit val noneInstance: Eq[None.type] = atomInstance[None.type]
+  implicit val intInstance:    Eq[Int]       = atomInstance[Int]
+  implicit val stringInstance: Eq[String]    = atomInstance[String]
+  implicit val noneInstance:   Eq[None.type] = atomInstance[None.type]
 
   implicit def listInstance[T: Eq]: Eq[Seq[T]] = new Eq[Seq[T]] {
     def equiv(lft: Seq[T], rgt: Any): Boolean = rgt match {
-      case x: Seq[T] =>
+      case x: Seq[_] =>
         val eq = implicitly[Eq[T]]
         lft.size == x.size && lft.zip(x).forall(pair => eq.equiv(pair._1, pair._2))
       case _ =>
@@ -37,9 +43,12 @@ object EqInstances {
 
   implicit def mapInstance[K: Eq, T: Eq]: Eq[Map[K, T]] = new Eq[Map[K, T]] {
     def equiv(lft: Map[K, T], rgt: Any): Boolean = rgt match {
-      case x: Map[K, T] =>
+      case x: Map[_, _] =>
         val keysEq = implicitly[Eq[K]]
         val valuesEq = implicitly[Eq[T]]
+
+        // В таких случаях лучше ввести две-три лишние переменные и разбить выражение для лучшей читаемости
+        // или лучше оставить вот так для краткости кода?
         lft.size == x.size && lft.keySet.zip(x.keySet).forall(keysPair => keysEq.equiv(keysPair._1, keysPair._2)) &&
         lft.values.zip(x.values).forall(valuesPair => valuesEq.equiv(valuesPair._1, valuesPair._2))
       case _ =>
@@ -53,3 +62,5 @@ object EqSyntax {
     def ====(other: Any): Boolean = implicitly[Eq[T]].equiv(self, other)
   }
 }
+
+object Eq extends EqInstances
